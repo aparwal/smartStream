f61ee1a44528da08fa9689c661ba51efe5199769
Anand Parwal <parwal.parwal@gmail.com>
2018-01-13 17:26:15 -0500

    add catch_stream.py

---
+++ catch_stream.py
@@ -0,0 +1,31 @@
+# Using Android IP Webcam video .jpg stream (tested) in Python2 OpenCV3
+
+import urllib.request
+import cv2
+import numpy as np
+import time
+
+# Replace the URL with your own IPwebcam shot.jpg IP:port
+url='http://130.215.122.188:8080/shot.jpg'
+
+
+while True:
+    # Use urllib to get the image from the IP camera
+    imgResp = urllib.request.urlopen(url)
+    
+    # Numpy to convert into a array
+    imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
+    
+    # Finally decode the array to OpenCV usable format ;) 
+    img = cv2.imdecode(imgNp,-1)
+	
+	
+	# put the image on screen
+    cv2.imshow('IPWebcam',img)
+
+    #To give the processor some less stress
+    #time.sleep(0.1) 
+
+    # Quit if q is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
